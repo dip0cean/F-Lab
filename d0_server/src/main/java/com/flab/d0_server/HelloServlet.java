e81da1da3c49a06ac6077e2f20de58812ae6016b
SeongHeon Sim <sh.sim@monthlything.com>
2021-07-04 18:49:10 +0900

    Servlet 테스트

--- d0_server/src/main/java/com/flab/d0_server/HelloServlet.java
+++ d0_server/src/main/java/com/flab/d0_server/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.flab.d0_server;
 
 import java.io.*;
+import java.util.List;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
@@ -20,6 +21,17 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+
+        // Request.getSession()
+        /*
+            현재 요청과 관련된 세션 객체를 리턴한다.
+            만약 세션이 존재하지 않는 경우, 새로운 세션을 생성해서 리턴한다.
+
+            세션 객체가 생성된 경우(새로 만들지 않는 경우)에만 세션을 불러오고 싶을 때는 getSession(false); 메소드 사용
+         */
+        HttpSession  session = request.getSession();
+        List<String> ids     = (List<String>) session.getAttribute("ids"); // 해당 세션에 저장된 ids 객체를 받아 온다.
+        getServletContext();
     }
 
     public void destroy() {
